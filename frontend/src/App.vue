46289df3d808e7ad82f23cd606d731aa64434e10
Uche Okereke <orezpablo@gmail.com>
2024-11-22 17:55:37 +0100

    send longURL to backend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,30 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import axios from "axios"
 
 const longUrl = ref("");
+const shortUrl = ref("")
+
+const shortenURL = async () => {
+    try {
+        const response = await axios.post("http://127.0.0.1:5000/api/urls/", {url: longUrl.value})
+        shortUrl.value = response.data.short_url
+
+    } catch (error) {
+        console.error("Error sending data:",error)
+
+    }
+}
 </script>
 
 <template>
   <div id="app">
     <h1>URL Shortener</h1>
-    <form>
+    <form @submit.prevent="shortenURL">
       <input v-model="longUrl" type="text" placeholder="Enter a long URL here" required />
       <button type="submit">Shorten URL</button>
     </form>
+
   </div>
 </template>
 
